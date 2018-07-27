cc01958dd5ad462c34236b15ac354e383ffbe57f
yuqiuju829 <451169158@qq.com>
2018-07-27 10:11:25 +0800

    打折券設置

---
+++ src/components/newBuild.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="newBuild">
+        <h1>{{title}}</h1>
+        <div class="btn">
+            <el-button type="warning" @click="newBuild" v-if="isAdd" id="new"><i class="el-icon-plus"></i> 新建</el-button>
+            <el-button type="warning" @click="save" v-else id="save">保存</el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"newBuild",
+    props:{
+        title:{
+            type:String,
+            default:"这是列表名称"
+        },
+        isAdd:{
+            type:Boolean,
+            default:"true"
+        }
+    },
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        newBuild(){
+            console.log("新建")
+        //    this.$emit('search',this.inputText)
+       },
+       save(){
+           console.log("保存")
+        //    this.$emit("date",this.date)
+       }
+    }
+}
+</script>
+
+<style>
+.newBuild{
+    width: 100%;
+    height: 90px;
+    background-color: #fff;
+    margin: 10px 0 20px;
+}
+.newBuild h1{
+    font-weight: bold;
+    font-size: 20px;
+    letter-spacing: 3px;
+    border-left: 2px solid #f99e1b;
+    padding-left: 10px;
+    float: left;
+    margin-top: 30px;
+}
+.btn{
+    width:100px;
+    float: right;
+}
+#new,#save{
+    margin-top: 25px;
+    border-radius: 25px;
+}
+</style>
--- src/pages/discountSite/discountSite.vue
+++ src/pages/discountSite/discountSite.vue
@@ -1,20 +1,62 @@
 <template>
     <div>
-        <div>打折券設置</div>
+        <newBuild @newBulid="newBuild" :isAdd="true" :title="title"></newBuild>
+        <div class="list">
+            <div class="left">
+                <img src="../../../static/header.jpg" alt="">
+                <div >
+                    <ul>
+                        <li>
+                            <p>代金券</p>
+                            <span>進行中</span>
+                        </li>
+                        <li>滿50送5圓代金券</li>
+                        <li>剩餘數量：77</li>
+                        <li>2018-02-25--2018-03-25</li>
+                    </ul>
+                </div>
+            </div>
+            <el-button type="danger">危险按钮</el-button>
+        </div>
     </div>
 </template>
 
 <script>
+import newBuild from "../../components/newBuild"
 export default {
     name:'discountSite',
+    components:{
+        newBuild
+    },
     data(){
         return{
-            
+            title:"打折券設置"
         }
     }
 }
 </script>
 
-<style>
 
+<style scoped>
+    .list{
+        display: flex;
+        justify-content: space-between;
+        width:100%;
+        height: 160px;
+        background-color: #fff;
+        margin-bottom: 20px;
+
+    }
+    .list img{
+        width: 120px;
+        height: 120px;
+    }
+    .list .left{
+        display: flex;
+        justify-content: space-around;
+        width:340px;
+        height: 120px;
+        background-color: blue;
+        margin: 20px;
+    }
 </style>
